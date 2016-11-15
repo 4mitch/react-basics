a5a2c307086988cd0c6b831f1f7bc4f3d1a49d3e
Oleg Polyakov <olegpolyakov@outlook.com>
2016-11-15 18:02:49 +0300

    Handling event

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,11 +7,17 @@ class Form extends React.Component {
         super(props);
 
         this.state = {};
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
     }
 
     render() {
         return (
-            <form className="todo-form">
+            <form className="todo-form" onSubmit={this.handleSubmit}>
                 <input type="text" placeholder="Что нужно сделать?" />
 
                 <Button type="submit">Добавить</Button>
