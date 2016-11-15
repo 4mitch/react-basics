b68a559959a6a0ace40725d22f72d14e92aaa668
Oleg Polyakov <olegpolyakov@outlook.com>
2016-11-15 16:58:44 +0300

    Updating state

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -5,17 +5,29 @@ class Checkbox extends React.Component {
         super(props);
 
         this.state = {
-            checked: false
+            checked: this.props.initiallyChecked
         };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(event) {
+        this.setState({
+            checked: !this.state.checked
+        });
     }
 
     render() {
         return (
-            <button className="checkbox icon">
+            <button className="checkbox icon" onClick={this.handleClick}>
                 <i className="material-icons">{this.state.checked ? 'check_box' : 'check_box_outline_blank'}</i>         
             </button>
         );
     }
 }
+
+Checkbox.propTypes = {
+    initiallyChecked: React.PropTypes.bool.isRequired
+};
 
 export default Checkbox;
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -6,7 +6,7 @@ import Button from './Button';
 function Todo(props) {
     return (
         <div className={`todo${props.completed ? ' completed' : ''}`}>
-            <Checkbox />
+            <Checkbox initiallyChecked={props.completed} />
 
             <span className="todo-title">{props.title}</span>
 
