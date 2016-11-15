1f5168011ae9b5076020bedf4c2bb1c6a3862e11
Oleg Polyakov <olegpolyakov@outlook.com>
2016-11-15 14:04:15 +0300

    Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = React.createElement('h1', null, 'React Todo');
-const subtitle = React.createElement('p', { className: 'subtitle' }, 'Это мое первое приложение на React');
-const container = React.createElement('div', null, title, subtitle);
-const app = (
-    <div>
-        <h1>React Todo</h1>
-        <p className="subtitle">Это мое первое приложение на React</p>
-    </div>
-);
+function App() {
+    return (
+        <div>
+            <h1>React Todo</h1>
+            <p className="subtitle">Это мое первое приложение на React</p>
+        </div>
+    );
+}
 
-console.log(title);
-console.log(subtitle);
-console.log(container);
-
-const dom = ReactDOM.render(app, document.getElementById('root'));
-console.log(dom);
+ReactDOM.render(<App />, document.getElementById('root'));
