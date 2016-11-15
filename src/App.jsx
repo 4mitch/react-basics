d202dc07598083fa66be6dc0d6b5f932d6d8bdbb
Oleg Polyakov <olegpolyakov@outlook.com>
2016-11-15 14:25:46 +0300

    Creating mockup

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,37 @@ import ReactDOM from 'react-dom';
 
 function App() {
     return (
-        <div>
-            <h1>React Todo</h1>
-            <p className="subtitle">Это мое первое приложение на React</p>
-        </div>
+        <main>
+            <header>
+                <h1>React Todo</h1>
+            </header>
+
+            <section className="todo-list">
+                <div className="todo completed">
+                    <button className="checkbox icon">
+                        <i className="material-icons">check_box</i>
+                    </button>
+
+                    <span className="todo-title">Изучить JavaScript</span>
+
+                    <button className="delete icon">
+                        <i className="material-icons">delete</i>
+                    </button>
+                </div>
+
+                <div className="todo">
+                    <button className="checkbox icon">
+                        <i className="material-icons">check_box_outline_blank</i>
+                    </button>
+
+                    <span className="todo-title">Изучить React</span>
+
+                    <button className="delete icon">
+                        <i className="material-icons">delete</i>
+                    </button>
+                </div>
+            </section>
+        </main>
     );
 }
 
